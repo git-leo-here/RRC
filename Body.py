68536428563c007ce2ee3ec22d78d3ab1e122631
Arya <aryaman77k7@gmail.com>
2021-12-24 17:18:27 +0530

    .

--- Body.py
+++ Body.py
@@ -2,7 +2,7 @@ import os
 import platform
 
 global listStd #Making ListStd As Super Global Variable
-listStd = ["yugesh", "kishor", "gajen", "Gopi"] #List Of Students
+listStd = ["arya", "ankit", "ankush"] #List Of Students
 
 def manageStudent(): #Function For The Student Management System
 
