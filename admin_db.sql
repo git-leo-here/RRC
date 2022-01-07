4d08608cc29d884581cc27cbc567c2f4cc46dbf3
Leo <neotheone458@gmail.com>
2022-01-07 13:33:31 +0530

    admin tables created

--- admin_db.sql
+++ admin_db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `admin_db`;
-USE `admin_db`;
-CREATE TABLE IF NOT EXISTS 'admin_db'.'admin_teachers' ( Teacher_ID INT AUTO_INCREMENT , Name char(20) , Password char(20) );
-
-DESC admin_teachers;
+CREATE DATABASE IF NOT EXISTS School;
+USE School;
+CREATE TABLE adminTeachers( TeacherID INT AUTO_INCREMENT , Name VARCHAR(32) NOT NULL , password VARCHAR(12) NOT NULL , PRIMARY KEY (TeacherID));
+CREATE TABLE adminStudents( StudentID INT AUTO_INCREMENT , StudentName VARCHAR(32) NOT NULL , GuardianName CHAR(32) NOT NULL , DOB DATE , PRIMARY KEY (StudentID));
+CREATE TABLE adminMain( ClassSec varchar(3), TeacherID int , subjects SET('P','C','M','B') NOT NULL , PRIMARY KEY (ClassSec) , FOREIGN KEY (TeacherID) REFERENCES adminTeachers(TeacherID) );
