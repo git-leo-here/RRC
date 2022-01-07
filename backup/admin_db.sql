57b14769711f5ca5c9aedb51b8a8d350de0d343b
Arya <aryaman77k7@gmail.com>
2022-01-07 21:14:46 +0530

    . hata diya

--- backup/admin_db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS School;
-USE School;
-CREATE TABLE IF NOT EXISTS adminTeachers( TeacherID INT AUTO_INCREMENT , Name VARCHAR(32) NOT NULL , password VARCHAR(12) NOT NULL , PRIMARY KEY (TeacherID));
-CREATE TABLE IF NOT EXISTS adminStudents( StudentID INT AUTO_INCREMENT , StudentName VARCHAR(32) NOT NULL , GuardianName CHAR(32) NOT NULL , DOB DATE , PRIMARY KEY (StudentID));
-CREATE TABLE IF NOT EXISTS adminMain( ClassSec varchar(3), TeacherID int , subjects SET('P','C','M','B') NOT NULL , PRIMARY KEY (ClassSec) , FOREIGN KEY (TeacherID) REFERENCES adminTeachers(TeacherID) );
