5d3f16caa0a679b9a952d1f8e7e71d6b56d4a528
Arya <aryaman77k7@gmail.com>
2022-01-21 11:30:44 +0530

    .

--- admin_db.sql
+++ admin_db.sql
@@ -1,5 +1,28 @@
-CREATE DATABASE IF NOT EXISTS School;
-USE School;
-CREATE TABLE IF NOT EXISTS adminTeachers( TeacherID INT AUTO_INCREMENT , Name VARCHAR(32) NOT NULL , password VARCHAR(12) NOT NULL , PRIMARY KEY (TeacherID));
-CREATE TABLE IF NOT EXISTS adminStudents( StudentID INT AUTO_INCREMENT , StudentName VARCHAR(32) NOT NULL , GuardianName CHAR(32) NOT NULL , DOB DATE , PRIMARY KEY (StudentID));
-CREATE TABLE IF NOT EXISTS adminMain( ClassSec varchar(3), TeacherID int , subjects SET('P','C','M','B') NOT NULL , PRIMARY KEY (ClassSec) , FOREIGN KEY (TeacherID) REFERENCES adminTeachers(TeacherID) );
+desc student;
++---------+-------------+------+-----+---------+----------------+
+| Field   | Type        | Null | Key | Default | Extra          |
++---------+-------------+------+-----+---------+----------------+
+| admno   | int(11)     | NO   | PRI | NULL    | auto_increment |
+| name    | varchar(30) | YES  |     | NULL    |                |
+| fname   | varchar(30) | YES  |     | NULL    |                |
+| class   | varchar(15) | YES  |     | NULL    |                |
+| section | varchar(10) | YES  |     | NULL    |                |
+| status  | char(15)    | YES  |     | NULL    |                |
++---------+-------------+------+-----+---------+----------------+
+
+Students ka taale banana ke liye , tu alag se bana lia hai toh koi baat nhi and jab tu banega table toh jo jo chiz autoincrement main rehne ka zaroorat hai for example adm no. who sab apne aap hojaaega I guess
+
+desc marks;
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| admno   | int(11)     | YES  |     | NULL    |       |
+| term    | varchar(20) | YES  |     | NULL    |       |
+| session | varchar(20) | YES  |     | NULL    |       |
+| phy     | int(3)      | YES  |     | NULL    |       |
+| chem    | int(3)      | YES  |     | NULL    |       |
+| math    | int(3)      | YES  |     | NULL    |       |
+| eng     | int(3)      | YES  |     | NULL    |       |
+| comp    | int(3)      | YES  |     | NULL    |       |
++---------+-------------+------+-----+---------+-------+
+Yes marks display ka sample query
